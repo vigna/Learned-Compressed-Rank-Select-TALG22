5882f36513ba8c90a5f18c356da7cb53f90e8469
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2026-04-17 08:32:14 +0200

    C++-equivalent averaging

--- run_all.sh
+++ run_all.sh
@@ -4,6 +4,7 @@ DIR="results"
 # change the path
 DIR_data="/usr/ALENEX_datasets"
 EXE_FILE="build/my_benchmark"
+SUX_FILE="sux-bench/${CARGO_TARGET_DIR:-target}/release/sux-bench"
 
 OPTIONS="--meva --plai --elia --rrrv --rlev --lave --lavo --encv --ds2i --s18v"
 
@@ -58,4 +59,49 @@ time {
   cat $DIR/DNA_5GRAM_URL_comparison.csv >$DIR/comparison.csv
   cat $DIR/GOV2_averages.csv >>$DIR/comparison.csv
   sed -n '6p' <$DIR/GOV2_10M-_comparison.csv >>$DIR/comparison.csv
+
+  $SUX_FILE $DIR_data/DNA_1 $DIR_data/DNA_2 $DIR_data/DNA_3 \
+    $DIR_data/5GRAM_1 $DIR_data/5GRAM_2 $DIR_data/5GRAM_3 \
+    $DIR_data/URL_1 $DIR_data/URL_2 $DIR_data/URL_3 \
+    >>$DIR/sux_DNA_5GRAM_URL_comparison.csv \
+    2>>$DIR/comparison.err
+
+  $SUX_FILE $DIR_data/GOV2/100K-1M/* \
+    >>$DIR/sux_GOV2_100K-1M_comparison.csv \
+    2>>$DIR/comparison.err
+
+  $SUX_FILE $DIR_data/GOV2/1M-10M/* \
+    >>$DIR/sux_GOV2_1M-10M_comparison.csv \
+    2>>$DIR/comparison.err
+
+  $SUX_FILE $DIR_data/GOV2/10M-/* \
+    >>$DIR/sux_GOV2_10M-_comparison.csv \
+    2>>$DIR/comparison.err
+
+  SUX_NUM_COLUMNS=$(head -1 $DIR/sux_GOV2_10M-_comparison.csv | sed 's/[^,]//g' | wc -c)
+
+  printf "'GOV2_AVG_10M-'" >>$DIR/sux_GOV2_averages.csv
+  for i in $(seq 2 "$SUX_NUM_COLUMNS"); do
+    printf "," >>$DIR/sux_GOV2_averages.csv
+    awk -v var="$i" -F ',' '{ total += $var } END { printf "%.3e", total/(NR-1) }' $DIR/sux_GOV2_10M-_comparison.csv >>$DIR/sux_GOV2_averages.csv
+  done
+  printf "\n" >>$DIR/sux_GOV2_averages.csv
+
+  printf "'GOV2_AVG_1M-10M'" >>$DIR/sux_GOV2_averages.csv
+  for i in $(seq 2 "$SUX_NUM_COLUMNS"); do
+    printf "," >>$DIR/sux_GOV2_averages.csv
+    awk -v var="$i" -F ',' '{ total += $var } END { printf "%.3e", total/(NR-1) }' $DIR/sux_GOV2_1M-10M_comparison.csv >>$DIR/sux_GOV2_averages.csv
+  done
+  printf "\n" >>$DIR/sux_GOV2_averages.csv
+
+  printf "'GOV2_AVG_100K-1M'" >>$DIR/sux_GOV2_averages.csv
+  for i in $(seq 2 "$SUX_NUM_COLUMNS"); do
+    printf "," >>$DIR/sux_GOV2_averages.csv
+    awk -v var="$i" -F ',' '{ total += $var } END { printf "%.3e", total/(NR-1) }' $DIR/sux_GOV2_100K-1M_comparison.csv >>$DIR/sux_GOV2_averages.csv
+  done
+  printf "\n" >>$DIR/sux_GOV2_averages.csv
+
+  cat $DIR/sux_DNA_5GRAM_URL_comparison.csv >$DIR/sux_ef_comparison.csv
+  cat $DIR/sux_GOV2_averages.csv >>$DIR/sux_ef_comparison.csv
+  sed -n '6p' <$DIR/sux_GOV2_10M-_comparison.csv >>$DIR/sux_ef_comparison.csv
 }
